d0104a89ee4fa966bb336772e3f643deaab01cfc
Mark Allibone <mark.allibone@rey-technology.com>
2024-02-20 10:21:43 +0100

    Adds slides and demos from MVU Who talk

---
+++ _Sessions_/MarkAllibone/Demos/BlobReactorMaui.Core/WeatherService.cs
@@ -0,0 +1,47 @@
+﻿using Akavache;
+using System.Reactive.Linq;
+using System.Text.Json;
+
+namespace BlobReactorMaui.Core;
+
+public class WeatherService : IWeatherService
+{
+    private readonly HttpClient _httpClient;
+    private const string BackendUrl = "https://mauireactivewebapi.azurewebsites.net/api/weatherforecast/forpostalcode/{0}";
+
+    public WeatherService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    private IObservable<WeatherForecast> GetWeatherForecast(int postalCode)
+    {
+        var url = string.Format(BackendUrl, postalCode);
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+        return
+            Observable.StartAsync(() => _httpClient.GetStringAsync(url))
+            .Select(forecast => JsonSerializer.Deserialize<WeatherForecast>(forecast, options)!);
+    }
+    public IObservable<WeatherForecast> GetWeatherForecastNoCache(int postalCode)
+    {
+        return GetWeatherForecast(postalCode);
+    }
+
+    public IObservable<WeatherForecast> GetWeatherForecastCache(int postalCode)
+    {
+        return
+
+        // Expiration Cache (the classic)
+        BlobCache.InMemory.GetOrFetchObject(
+            $"forecast{postalCode}",
+            () => GetWeatherForecast(postalCode),
+            DateTimeOffset.Now.AddSeconds(5))!;
+
+        // Get and fetch latest
+        // BlobCache.LocalMachine.GetAndFetchLatest($"forecast{postalCode}",
+        //     () => GetWeatherForecast(postalCode))!;
+    }
+}
